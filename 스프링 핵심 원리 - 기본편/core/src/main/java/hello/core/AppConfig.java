31ea48b5b29d8c2f7b868a26166b92d7f088035f
stk346 <stk346@gamil.com>
2022-12-19 21:06:20 +0900

    @Configuration과 싱글톤

--- 스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/AppConfig.java
+++ 스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/AppConfig.java
@@ -14,19 +14,25 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
+    // @Bean -> memberService -> new MemoryMemberRepository()
+    // @Bean -> orderService -> new MemoryMemberRepository()
+    // 이렇게 되면 각각의 객체가 생성되면서 싱글톤이 깨지는 것처럼 보인다.
 
     @Bean
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemberRepository  memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
--- 스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ 스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,6 +3,7 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
 
 public class   OrderServiceImpl implements OrderService {
 
@@ -20,5 +21,10 @@ public class   OrderServiceImpl implements OrderService {
         int discountPrice = discountPolicy.discount(member, itemPrice);
 
         return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+
+    // 테스트 용도
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
     }
 }

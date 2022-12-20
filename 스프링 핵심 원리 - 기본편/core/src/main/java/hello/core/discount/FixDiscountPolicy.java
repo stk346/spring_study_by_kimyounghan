8bcc30f060d1921ff93d4fa28172f39a8f08a6fd
stk346 <stk346@gamil.com>
2022-12-20 18:51:14 +0900

    @Autowired 필드 명, @Qualifier, @Primary

--- 스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ 스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -2,6 +2,8 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component

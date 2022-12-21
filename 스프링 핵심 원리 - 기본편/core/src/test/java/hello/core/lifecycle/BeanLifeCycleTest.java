8362249fc50b69013f75e73c56c9b56d9e77538c
stk346 <stk346@gamil.com>
2022-12-21 09:33:22 +0900

    빈 생성주기: 인터페이스 InitializingBean, DisposableBean로 확인하는 방법.

---
+++ 스프링 핵심 원리 - 기본편/core/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,30 @@
+package hello.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    public void lifeCycleTest() {
+        // 보통 close() 메서드를 쓸 일이 없기 때문에 상위 객체로 받아야 호출이 가능하게 돼있음.
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-spring.dev");
+            return networkClient;
+        }
+    }
+}

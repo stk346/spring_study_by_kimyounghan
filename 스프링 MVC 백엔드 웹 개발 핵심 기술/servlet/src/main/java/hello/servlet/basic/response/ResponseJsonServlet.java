f14d9c3f0768ca4ef4ead68365065ec54bc3b661
stk346 <stk346@gamil.com>
2022-12-22 22:09:31 +0900

    HTTP 응답 데이터 - API JSON

--- 스프링 MVC 백엔드 웹 개발 핵심 기술/servlet/src/main/java/hello/servlet/basic/response/ResponseJsonServlet.java
+++ 스프링 MVC 백엔드 웹 개발 핵심 기술/servlet/src/main/java/hello/servlet/basic/response/ResponseJsonServlet.java
@@ -1,4 +1,32 @@
 package hello.servlet.basic.response;
 
-public class ResponseJsonServlet {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.servlet.basic.HelloData;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "responseJsonServlet", urlPatterns = "/response-json")
+public class ResponseJsonServlet extends HttpServlet {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Content-Type: application/json
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+
+        HelloData hellodata = new HelloData();
+        hellodata.setUsername("kim");
+        hellodata.setAge(20);
+
+        //{"username":"kim", "age":20}
+        String result = objectMapper.writeValueAsString(hellodata);
+        response.getWriter().write(result);
+    }
 }

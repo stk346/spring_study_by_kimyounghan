d476876218ac69facb89eb4111a210582ccf7e65
stk346 <stk346@gamil.com>
2022-12-26 10:13:06 +0900

    스프링 MVC - 컨트롤러 통합

--- 스프링 MVC 백엔드 웹 개발 핵심 기술/servlet/src/main/java/hello/servlet/web/springmvc/v2/SpringMemberControllerV2.java
+++ 스프링 MVC 백엔드 웹 개발 핵심 기술/servlet/src/main/java/hello/servlet/web/springmvc/v2/SpringMemberControllerV2.java
@@ -1,23 +1,28 @@
-package hello.servlet.web.springmvc.v1;
+package hello.servlet.web.springmvc.v2;
 
 import hello.servlet.domain.member.Member;
 import hello.servlet.domain.member.MemberRepository;
-import hello.servlet.web.frontcontroller.ModelView;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Map;
+import java.util.List;
 
 @Controller
-public class SpringMemberSaveControllerV1 {
+@RequestMapping("/springmvc/v2/members")
+public class SpringMemberControllerV2 {
 
     private MemberRepository memberRepository = MemberRepository.getInstance();
 
-    @RequestMapping("/springmvc/v1/members/save")
-    public ModelAndView process(HttpServletRequest request, HttpServletResponse response) {
+    @RequestMapping("/new-form")
+    public ModelAndView newForm() {
+        return new ModelAndView("new-form");
+    }
+
+    @RequestMapping("/save")
+    public ModelAndView save(HttpServletRequest request, HttpServletResponse response) {
         String username = request.getParameter("username");
         int age = Integer.parseInt(request.getParameter("age") );
 
@@ -28,4 +33,15 @@ public class SpringMemberSaveControllerV1 {
         mv.addObject("member", member);
         return mv;
     }
+
+    @RequestMapping
+    public ModelAndView members() {
+        List<Member> members = memberRepository.findAll();
+        ModelAndView mv = new ModelAndView("members");
+        mv.addObject("members", members);
+
+        return mv;
+    }
+
+
 }

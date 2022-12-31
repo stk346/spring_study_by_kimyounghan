552d4c6f3b8b897d78d2c8da560923ea42223ed2
stk346 <stk346@gamil.com>
2022-12-31 11:44:59 +0900

    Form 전송 객체 분리 - 개발 (SaveForm, UpdateForm 으로 분리)

--- 스프링 MVC 백엔드 웹 개발 핵심 기술2/validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ 스프링 MVC 백엔드 웹 개발 핵심 기술2/validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -10,16 +10,16 @@ import javax.validation.constraints.NotNull;
 @Data
 public class Item {
 
-    @NotNull(groups = UpdateCheck.class) // 아이템을 수정할 때 사용
+//    @NotNull(groups = UpdateCheck.class) // 아이템을 수정할 때 사용
     private Long id;
-    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
+//    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
 
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Max(value = 9999, groups = {SaveCheck.class})
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Max(value = 9999, groups = {SaveCheck.class})
     private Integer quantity;
 
     public Item() {

36944397db8b07a7a4ae9655bc0da31d0acbbe65
stk346 <stk346@gamil.com>
2022-12-31 18:44:10 +0900

    서블릿 필터 - 인증 체크

---
+++ 스프링 MVC 백엔드 웹 개발 핵심 기술2/login/src/main/java/hello/login/web/filer/LoginCheckFilter.java
@@ -0,0 +1,55 @@
+package hello.login.web.filer;
+
+import hello.login.web.SessionConst;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.PatternMatchUtils;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@Slf4j
+public class LoginCheckFilter implements Filter {
+
+    private static final String[] whiteList = {"/", "/members/add", "/login", "/logout", "/css/*"};
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String requestURI = httpRequest.getRequestURI();
+
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        try {
+            log.info("인증 체크 필터 시작{}", requestURI);
+
+            if (isLoginCheckPath(requestURI)) {
+                log.info("인증 체크 로직 실행{}", requestURI);
+                HttpSession session = httpRequest.getSession(false);
+                if (session == null || session.getAttribute(SessionConst.LOGIN_MEMBER) == null) {
+
+                    log.info("미인증 사용자 요청{}", requestURI);
+                    // 로그인 페이지로 리다이렉트 & 현재 페이지 정보를 같이 넘겨 로그인 됐을 때 해당 페이지로 이동할 수 있도록 함
+                    httpResponse.sendRedirect("/login?redirectURL=" + requestURI);
+                    return;
+                }
+            }
+
+            chain.doFilter(request, response);
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            log.info("인증 체크 필터 종료 {}", requestURI);
+        }
+    }
+
+    /**
+     * 화이트 리스트의 경우 인증 체크 x
+     */
+    public boolean isLoginCheckPath(String requestURI) {
+        return !PatternMatchUtils.simpleMatch(whiteList, requestURI);
+    }
+}

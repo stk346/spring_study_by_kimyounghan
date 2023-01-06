19fb33087ed483cd7df507d5662b5b42ca0c6e47
stk346 <stk346@gamil.com>
2023-01-06 11:30:39 +0900

    엔티티 클래스 개발2

--- JAP활용1/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ JAP활용1/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
@@ -21,4 +23,8 @@ public abstract class Item {
     private int price;
 
     private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
+
 }

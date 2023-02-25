e538876b7fd0dad52f85dfa1e97c17251073399a
stk346 <stk346@gamil.com>
2023-02-25 12:50:06 +0900

    공통 인터페이스 기능 확인

--- springDataJpa/data-jpa/src/main/java/study/datajpa/DataJpaApplication.java
+++ springDataJpa/data-jpa/src/main/java/study/datajpa/DataJpaApplication.java
@@ -1,7 +1,14 @@
 package study.datajpa;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import study.datajpa.entity.Member;
+import study.datajpa.entity.Team;
+import study.datajpa.repository.MemberJpaRepository;
+import study.datajpa.repository.MemberRepository;
+
+import javax.persistence.EntityManager;
 
 @SpringBootApplication
 public class DataJpaApplication {

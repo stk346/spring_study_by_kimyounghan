d009b1d73002e93a55c300ccc6279afd2ef58a09
stk346 <stk346@gamil.com>
2023-02-25 19:31:33 +0900

    네이티브 쿼리

--- springDataJpa/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ springDataJpa/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -59,4 +59,13 @@ public interface MemberRepository extends JpaRepository<Member, Long>, MemberRep
     List<Member> findLockByUsername(String username);
 
     List<UsernameOnly> findProjectionsByUsername(@Param("username") String username);
+
+    @Query(value = "select * from member where username = ?", nativeQuery = true)
+    Member findByNativeQuery(String username);
+
+    @Query(value = "select m.member_id as id, m.username, t.name as teamName " +
+            "from member m left join team t",
+            countQuery = "select count(*) from member",
+            nativeQuery = true)
+    Page<MemberProjection> findByNativeProjection(Pageable pageable);
 }

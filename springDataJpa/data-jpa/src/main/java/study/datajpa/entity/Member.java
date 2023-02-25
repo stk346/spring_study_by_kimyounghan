338ca419b9b3b04529cfd80066a11f38a012ad0a
stk346 <stk346@gamil.com>
2023-02-25 15:44:40 +0900

    쿼리 메소드 기능

--- springDataJpa/data-jpa/src/main/java/study/datajpa/entity/Member.java
+++ springDataJpa/data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -8,6 +8,10 @@ import javax.persistence.*;
 @Getter @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString(of = {"id", "username", "age"})
+@NamedQuery(
+        name="Member.findByUsername",
+        query="select m from Member m where m.username = :username"
+)
 public class Member {
 
     @Id @GeneratedValue
@@ -24,8 +28,13 @@ public class Member {
         this.username = username;
     }
 
-    public Member(String usernme, int age, Team team) {
-        this.username = usernme;
+    public Member(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
         this.age = age;
         if (team != null) {
             changeTeam(team);
